2f062f6344197aee2d30767504fc4d373f0b8be4
Jaozin04 <61639068+Jaozin04@users.noreply.github.com>
2022-06-02 17:07:01 -0300

    Update pwascript.js

--- js/pwascript.js
+++ js/pwascript.js
@@ -2,7 +2,7 @@ if ("serviceWorker" in navigator) {
 	window.addEventListener("load", function () {
 	const url = "https://github.com/Jaozin04/pwa";
 	  navigator.serviceWorker
-		.register(`${url}sw.js`)
+		.register(`${url}/js/sw.js`)
 		.then(res => console.log("service worker registered"))
 		.catch(err => console.log("service worker not registered", err))
 	})
